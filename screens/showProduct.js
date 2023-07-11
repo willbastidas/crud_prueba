375188b6e6064a353ecbda03a26ba27297690038
willbastidas <willkergaciab@gmail.com>
2023-07-11 11:30:38 -0400

    no funcional

--- screens/showProduct.js
+++ screens/showProduct.js
@@ -4,12 +4,41 @@ import * as React from 'react';
 import { Styles } from '../estilos/styles';
 import { createStackNavigator } from '@react-navigation/stack';
 import { NavigationContainer } from '@react-navigation/native';
+import { useState, useEffect} from 'react';
 
-export default function showProduct() {
+//importaciones de fira base
+import appFirebase from '../credenciales';
+import { getFirestore, collection, addDoc, getDocs, doc, deleteDoc, setDoc, getDoc} from 'firebase/firestore';
+
+const db = getFirestore(appFirebase);
+
+export default function showProduct(props) {
+
+  const [product, setProduct] = useState({});
+
+  const getOneProduct = async(id)=>  {
+    try {
+      const docRef = doc(db, 'product', id);
+      const docSnap = await getDoc(docRef);
+      setProduct(docSnap.data())
+    } catch{ console.error(error)
+      
+    }
+  }
+
+  useEffect(()=> {
+    getOneProduct(props.route.params.productID)
+  },[])
+
   return (
-    <View style={Styles.container}>
-      <Text>show product</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Text style={Styles.textoTitulo}>Detalles Del Producto</Text>
+
+
+      <Text>nombre:{product.nombre} </Text>
+      <Text>nombre:{product.Tipo_de_producto} </Text>
+      <Text>nombre:{product.Color_de_producto} </Text>
+      <Text>nombre:{product.stock} </Text>
     </View>
   );
 }
